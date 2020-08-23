6e7bed50ba73eb45b71095d9326921f0c8009e9c
Satyam1203 <nakulbegana191170@gmail.com>
2020-08-23 16:19:00 +0530

    Navbar, bg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 
-import Resume from './components/Resume';
+import Navbar from './components/Navbar';
+import Home from "./components/Home";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <div className="App">
-      <h1>Github Resume Generator</h1>
-      <Resume />
+      <Navbar />
+      <Home />
+      {/* <Resume /> */}
     </div>
   );
 }
