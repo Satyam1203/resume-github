46c45ef164c65d70654ed02ae715905cd4b500e1
Satyam1203 <nakulbegana191170@gmail.com>
2020-08-24 16:43:14 +0530

    Added Readme.md

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   // const token = process.env.REACT_APP_GRAPHQL_API;
-  const token = '';
+  const token = process.env.REACT_APP_GRAPHQL_API;
   return {
     headers: {
       ...headers,
