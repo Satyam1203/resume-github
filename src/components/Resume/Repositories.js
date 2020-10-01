ac320f3e896794d03d123dca7d4573b0ab0dc2c6
Iordanis Kostelidis <kostelidis@ieee.org>
2020-10-01 21:55:09 +0300

    refactor-optimize

--- src/components/Resume/Repositories.js
+++ src/components/Resume/Repositories.js
@@ -2,26 +2,32 @@ import React from "react";
 import Repo from "./Repo";
 import { Wrapper } from "./styles";
 
+function areUsernameOwnerOfRepo(username, repo) {
+    return repo.nameWithOwner.split("/")[0].toLowerCase() === username.toLowerCase()
+}
+
 function Repositories({ repoList, username }) {
   return (
     <Wrapper>
       <h2>Top Repositories</h2>
       <div style={{ margin: "1rem 0 0 1rem" }}>
         {repoList.map((repo) => {
-          if (repo.nameWithOwner.split("/")[0].toLowerCase() === username.toLowerCase())
+            if (!(areUsernameOwnerOfRepo(username, repo))) {
+                return null
+            }
+
             return (
-              <Repo
-                key={repo.id}
-                forks={repo.forkCount}
-                languages={repo.languages}
-                homepageUrl={repo.homepageUrl}
-                name={repo.name}
-                description={repo.shortDescriptionHTML}
-                stars={repo.stargazers.totalCount}
-                url={repo.url}
-              />
+                <Repo
+                    key={repo.id}
+                    forks={repo.forkCount}
+                    languages={repo.languages}
+                    homepageUrl={repo.homepageUrl}
+                    name={repo.name}
+                    description={repo.shortDescriptionHTML}
+                    stars={repo.stargazers.totalCount}
+                    url={repo.url}
+                />
             );
-          return null
         })}
       </div>
     </Wrapper>
